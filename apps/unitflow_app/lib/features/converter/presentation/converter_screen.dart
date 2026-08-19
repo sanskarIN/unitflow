a65745e5fe1505586eaa454be351dd2c97e94f73
Sanskar <sanskarin@outlook.in>
2026-08-19 12:03:57 +0530

    fix: synchronize programmatic converter input changes

--- apps/unitflow_app/lib/features/converter/presentation/converter_screen.dart
+++ apps/unitflow_app/lib/features/converter/presentation/converter_screen.dart
@@ -17,9 +17,25 @@ final class ConverterScreen extends StatefulWidget {
 }
 
 final class _ConverterScreenState extends State<ConverterScreen> {
-  late final TextEditingController _inputController = TextEditingController(
-    text: widget.controller.input,
-  );
+  late final TextEditingController _inputController;
+
+  @override
+  void initState() {
+    super.initState();
+    _inputController = TextEditingController(text: widget.controller.input);
+    widget.controller.addListener(_syncInputFromController);
+  }
+
+  @override
+  void didUpdateWidget(covariant ConverterScreen oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.controller == widget.controller) {
+      return;
+    }
+    oldWidget.controller.removeListener(_syncInputFromController);
+    widget.controller.addListener(_syncInputFromController);
+    _syncInputFromController();
+  }
 
   @override
   void didChangeDependencies() {
@@ -27,8 +43,20 @@ final class _ConverterScreenState extends State<ConverterScreen> {
     widget.controller.setLocale(Localizations.localeOf(context).toLanguageTag());
   }
 
+  void _syncInputFromController() {
+    final next = widget.controller.input;
+    if (_inputController.text == next) {
+      return;
+    }
+    _inputController.value = TextEditingValue(
+      text: next,
+      selection: TextSelection.collapsed(offset: next.length),
+    );
+  }
+
   @override
   void dispose() {
+    widget.controller.removeListener(_syncInputFromController);
     _inputController.dispose();
     super.dispose();
   }
