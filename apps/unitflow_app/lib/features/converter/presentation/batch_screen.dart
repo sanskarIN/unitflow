9c987a1c0009a05cff03b3cd773cafd316727376
Sanskar <sanskarin@outlook.in>
2026-08-21 06:35:49 +0530

    fix: synchronize batch input from shared state

--- apps/unitflow_app/lib/features/converter/presentation/batch_screen.dart
+++ apps/unitflow_app/lib/features/converter/presentation/batch_screen.dart
@@ -18,9 +18,25 @@ final class BatchScreen extends StatefulWidget {
 }
 
 final class _BatchScreenState extends State<BatchScreen> {
-  late final TextEditingController _inputController = TextEditingController(
-    text: widget.controller.input,
-  );
+  late final TextEditingController _inputController;
+
+  @override
+  void initState() {
+    super.initState();
+    _inputController = TextEditingController(text: widget.controller.input);
+    widget.controller.addListener(_syncInputFromController);
+  }
+
+  @override
+  void didUpdateWidget(covariant BatchScreen oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.controller == widget.controller) {
+      return;
+    }
+    oldWidget.controller.removeListener(_syncInputFromController);
+    widget.controller.addListener(_syncInputFromController);
+    _syncInputFromController();
+  }
 
   @override
   void didChangeDependencies() {
@@ -28,8 +44,20 @@ final class _BatchScreenState extends State<BatchScreen> {
     widget.controller.setLocale(Localizations.localeOf(context).toLanguageTag());
   }
 
+  void _syncInputFromController() {
+    final input = widget.controller.input;
+    if (_inputController.text == input) {
+      return;
+    }
+    _inputController.value = TextEditingValue(
+      text: input,
+      selection: TextSelection.collapsed(offset: input.length),
+    );
+  }
+
   @override
   void dispose() {
+    widget.controller.removeListener(_syncInputFromController);
     _inputController.dispose();
     super.dispose();
   }
